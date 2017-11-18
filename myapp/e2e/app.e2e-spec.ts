b250f9ef43af484629af5b69d38863b5064a7c02
Azza Rojbi <arojbi@my.bcit.ca>
2017-11-17 19:26:41 -0800

    setting up e2e testing

--- myapp/e2e/app.e2e-spec.ts
+++ myapp/e2e/app.e2e-spec.ts
@@ -1,14 +1,29 @@
 import { AppPage } from './app.po';
+import { browser, element, by } from 'protractor';
 
-describe('myapp App', () => {
+describe('testing application startup!!!', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Get more practice working with elements in page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+    // Ensure element with id='fname' has text equalling Jane.
+    expect(element(by.id('fname')).getText()).toEqual('Jane');
+  
+    // Get input element and enter the text 'Boston'.
+    var cityInput = element(by.id('cityInput'));
+    cityInput.sendKeys('Boston');
+  
+    // Get the button and click it.
+    var msgButton = element(by.id('showMsgButton'));
+    msgButton.click();
+  
+    // Test the message that is output after the button is clicked.
+    var messageOutput = element(by.id('messageLabel'));
+    expect(messageOutput.getText()).toEqual('It is always sunny in Boston');
+  })
 });
+
